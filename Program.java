462b5cb488bb542e8ab9eb020c1b870356ffab7f
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2023-02-27 23:02:07 +0700

    Это то?

---
+++ Program.java
@@ -0,0 +1,23 @@
+public class Program {
+    public static void main(String[] args) {
+
+        ListTask firstCalendar = new ListTask();
+        WorkTask workTask1 = new WorkTask("15:00", "16:00", "Высокий", "подготовить отчет", "Иванов И.И.");
+        PersonalTask personalTask1 = new PersonalTask("19:00", "19:30", "Средний", "Выгулять собаку");
+        WorkTask workTask2 = new WorkTask("16:00", "17:00", "Высокий", "Презентация отчета", "Иванов И.И.");
+        firstCalendar.addTask(workTask1);
+        firstCalendar.addTask(workTask2);
+        firstCalendar.addTask(personalTask1);
+        PersonalTask personalTask2 = new PersonalTask("20:00", "21:00", "Обычный", "Семейный ужин");
+        firstCalendar.addTask(personalTask2);
+        for (Task task : firstCalendar) {
+            System.out.println(task);
+        }
+        firstCalendar.deleteTask(workTask1);
+        System.out.println();
+        for (Task task : firstCalendar) {
+            System.out.println(task);
+        }
+    }
+    
+}
